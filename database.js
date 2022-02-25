ce3457651ce47d9a40957bf346fed2f3e7e93f5b
veronicaliljegren <liljegrenveronica@gmail.com>
2022-02-25 13:55:01 +0100

    The array is filled

---
+++ database.js
@@ -0,0 +1,119 @@
+"use strict"
+
+let database = [
+    {
+        id: 1,
+        name: "Pasta alla norma", 
+        typeOfPasta: "Rigatoni", 
+        time: 30,
+        difficulty: "Easy", 
+    }, 
+
+    {
+        id: 2,
+        name: "Pasta al linome", 
+        typeOfPasta: "Tagliatelle", 
+        time: 30, 
+        difficulty: "Easy", 
+
+    }, 
+
+    {
+        id: 3,
+        name: "Carbonara", 
+        typeOfPasta: "Spaghetti", 
+        time: 30, 
+        difficulty: "Easy", 
+
+    }, 
+
+    {
+        id: 4,
+        name: "Bolognese", 
+        typeOfPasta:"Mezze Maniche", 
+        time: 60, 
+        difficulty: "Medium", 
+
+    }, 
+
+    {
+        id: 5,
+        name: "Pasta puttanesca", 
+        typeOfPasta:"Spaghetti", 
+        time: 45, 
+        difficulty: "Medium", 
+
+    },
+
+    {
+        id: 6,
+        name: "Pasta vongole", 
+        typeOfPasta:"Linguine", 
+        time: 60, 
+        difficulty: "Hard", 
+
+    },
+
+    {
+        id: 7,
+        name: "Penne zafferano", 
+        typeOfPasta: "Penne", 
+        time: 25, 
+        difficulty: "Easy", 
+
+    },
+
+    {
+        id: 8,
+        name: "Cacio e pepe", 
+        typeOfPasta: "Bucatini", 
+        time: 10, 
+        difficulty:"Easy", 
+
+    },
+
+    {
+        id: 9,
+        name: "Pasta al tono", 
+        typeOfPasta:"pappardelle", 
+        time: 20, 
+        difficulty:"Easy", 
+
+    },
+
+    {
+        id: 10,
+        name: "Ragù alla genovese", 
+        typeOfPasta: "Penne lisce", 
+        time: 540, 
+        difficulty:"Hard", 
+
+    }
+]
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

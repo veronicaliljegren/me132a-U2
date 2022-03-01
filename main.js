944e80422025c73dc3fac7b08d11ee615435fb72
veronicaliljegren <liljegrenveronica@gmail.com>
2022-03-01 11:43:49 +0100

    Added functions to java
    function( addNewDishToDatabase) to add a new dish, function (removePastaByID)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 //Skapar en ny pastarätt
 function createNewDish (id, dish, typeOfPasta, time, difficulty){
@@ -12,22 +12,63 @@ function createNewDish (id, dish, typeOfPasta, time, difficulty){
     return pasta;
 }
 
-//Lägga till pastarätten i databasen
+
+//Lägga till en pastarätt i databasen
 function addNewDishToDatabase (pastaDatabase, pasta){
+    console.log (`You are adding ${pasta.dish} to the database`);
     pastaDatabase.push(pasta);
 }
 
+//Ta bort en pastarätt från databasen
+function removePastaByID(pastaDatabase, pasta){
+    for( let pasta of pastaDishes)
+    let pasta = pasta[i];
 
-// //Returns all pastadishes 
-// function pastaDishes(pastaDatabase, dish){
-//     let  = [];
+    if(pasta.id == id){
+        pasta.splice(i , 1);
+        return;
+    }
+}
 
-//     for (let pasta of pastaDatabase){
-//         pastaDatabase.push(pasta);
-//     }
-//     return dogsByBreed;
-// }
 
+//Returns all pastadishes 
+function pastaDishes(pastaDatabase, dish){
+    let pastaDish = [];
+
+    for (let pasta of pastaDatabase){
+        if (pasta.dish.toLowerCase()== dish.toLowerCase()){
+            pastaDatabase.push(pasta);
+        }
+       
+    }
+    return pastaDish;
+}
+
+//Returns pasta by time
+function pastaTime(pastaDatabase, time){
+    let pastaTime = [];
+
+    for (let pasta of pastaDatabase){
+        if (pasta.time.toLowerCase()== time.toLowerCase()){
+            pastaDatabase.push(pasta);
+        }
+       
+    }
+    return pastaTime;
+}
+
+//Returns pasta by difficulty 
+function pastaDifficulty(pastaDatabase, difficulty){
+    let pastaDifficulty = [];
+
+    for (let pasta of pastaDatabase){
+        if (pasta.difficulty.toLowerCase()== difficulty.toLowerCase()){
+            pastaDatabase.push(pasta);
+        }
+       
+    }
+    return pastaDifficulty;
+}
 
 //skapa div 
 function createDiv(pasta) {
@@ -55,8 +96,57 @@ function visiblePasta(){
         let pastaChart = document.getElementById("pastaChart");
         pastaChart.appendChild(pastaElement);
     }
+    setRemovePastaHandlers();
 }
+
+//Filter recepies by dish
+function onFilterByDish(event){
+    event.preventDefault();
+    // Vilken rätt?
+    let dish = document.getElementById("filter-dish").value;
+    //Välj recept beroende på rätt
+    let pastas = pastaDishes(pastaDatabase, dish);
+    // Visa rätterna som filtrerats beroende på rätt
+    visiblePasta(pastas);
+
+}
+//Filter recepies by time
+function onFilterByTime (event){
+    event.preventDefault(event)
+    //Vilken tid?
+    let time =document.getElementById("filter-time").value;
+    //Välj recept beroende på tid
+    let pastas = pastaTime(pastaDatabase, time);
+    //Visa rätterna som filtrerats beroende på tid
+    visiblePasta(pastas);
+
+}
+//Filter recepies by difficulty
+function onFilterByDifficulty(event){
+    event.preventDefault();
+    //Vilken svårighetsgrad
+    let difficulty = document.getElementById("filter-difficulty").value;
+    //Välj recept beroende på svårighetgrad
+    let pastas = pastaTime(pastaDatabase, difficulty);
+    //Visa rätterna som filtrerats beroende på svårighetsgrad
+    visiblePasta(pastas);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 visiblePasta();
+
 
 
 
